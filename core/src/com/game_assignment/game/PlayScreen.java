97ba2ba52cb7716dc5224505560d2eb6385c3b39
Shubham99530 <shubham21290@iiitd.ac.in>
2022-12-21 20:59:25 +0530

    changed some graphics and projectile inserted

--- core/src/com/game_assignment/game/PlayScreen.java
+++ core/src/com/game_assignment/game/PlayScreen.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+
+import java.security.PrivateKey;
 
 
 public class PlayScreen extends ScreenAdapter {
@@ -14,23 +17,25 @@ public class PlayScreen extends ScreenAdapter {
     Game_Assignment game;
     Texture tankA, tankB;
 
+    private float tankA_X =20  ;
+    private float tankA_Y = 65 ;
 
-    float tankA_X =20  ;
-    float tankA_Y = 65 ;
+    private float f=150;
+    private int flag =0;
+    private float fireball_A_X = tankA_X+100;
+    private float fireball_A_Y = tankA_Y+100;
+    private float x = fireball_A_X;
+    private float y =fireball_A_Y;
 
-    float f=150;
-    int flag =0;
-    float fireball_A_X = tankA_X+100;
-    float fireball_A_Y = tankA_Y+100;
-    float x = fireball_A_X;
-    float angle = 0;
-    float height = 0;
-    float range = 0;
-    float tankB_X=600-tankA_X;
-    float tankB_Y=tankA_Y;
-    double a =0;
-    float hA = tankA_X;
-     float hb = tankB_X;
+    private float power =0;
+    private float angle = 0;
+    private float height = 0;
+    private float range = 0;
+    private float tankB_X=600-tankA_X;
+    private float tankB_Y=tankA_Y;
+    private double a =0;
+    private float hA = tankA_X;
+    private float hb = tankB_X;
 
     public PlayScreen(Game_Assignment game , Texture tex ,Texture tankB) {
         this.game = game;
@@ -78,8 +83,8 @@ public class PlayScreen extends ScreenAdapter {
         game.font.draw(game.batch,"Tank Player 1 turn" , hA + 100, 395 );
         game.font.draw(game.batch,"Fuel ", hA,350);
 
-        game.font.draw(game.batch,"Angle of Projection :  "+angle, 150,200);
-
+        game.font.draw(game.batch,"Angle of Projection :  "+angle, 150,120);
+        game.font.draw(game.batch,"Power of Attack :" + power, 650, 120);
 
         game.batch.draw(tankA,tankA_X ,tankA_Y);
         game.batch.draw(tankB,tankB_X,tankB_Y);
@@ -115,17 +120,40 @@ public class PlayScreen extends ScreenAdapter {
                         angle -= 5;
                     }
                 }
-                if (Gdx.input.isKeyPressed(Input.Keys.ENTER))
-                {
-                    a= Math.toRadians(angle);
-                    range = (float) ((100*100)*(Math.sin(2*a))/9.8) +x ;
-                    if(fireball_A_X <= range) {
-                        fireball_A_X += 1;
-                    }
-                    fireball_A_Y = (float) ((fireball_A_X*Math.tan(a))-((9.8*(fireball_A_X*fireball_A_X))/(2*(10000)*(Math.cos(a)*Math.cos(a))))) + fireball_A_Y;
 
+                if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP))
+                {
+                    if(power <50f)
+                    {
+                        power+=1;
+                    }
                 }
-                flag = 1;
+                else if(Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN))
+                {
+                    if (power > 0)
+                    {
+                        power-=1;
+                    }
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+                    a = Math.toRadians(angle);
+
+                    range = (float) ((power * power * 100) * (Math.sin(2 * a)) / 9.8) + x;
+                    height = (float) ((10000) * (Math.sin(a) * Math.sin(a)) / 2 * 9.8);
+                    if (fireball_A_X < range) {
+
+                        if (fireball_A_X < tankB_X - 50 && fireball_A_Y < tankB_Y - 50) {
+                            flag = 1;
+                        }
+                        fireball_A_X += 10;
+
+                        fireball_A_Y = (float) ((fireball_A_X * Math.tan(a)) - ((9.8 * (fireball_A_X * fireball_A_X)) / (2 * (power * power * 100) * (Math.cos(a) * Math.cos(a))))) + 165;
+
+                    } else {
+                        flag = 1;
+                    }
+//
+                }
             }
             else if(flag == 1)
             {
@@ -134,7 +162,7 @@ public class PlayScreen extends ScreenAdapter {
                 } else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
                     tankB_X += 3;
                 }
-                flag = 0;
+//                flag = 0;
             }
 
             if(Gdx.input.isKeyPressed(Input.Keys.SPACE))
@@ -147,12 +175,15 @@ public class PlayScreen extends ScreenAdapter {
         game.batch.end();
         game.shapeRenderer.end();
     }
-
-
-
+//    public float fireball(float fireball_A_X, float power)
+//    {
+//        float y =(float) ((fireball_A_X * Math.tan(a)) - ((9.8 * (fireball_A_X * fireball_A_X)) / (2 * (power*power*10) * (Math.cos(a) * Math.cos(a)))));
+//        return y;
+//    }
     @Override
     public void hide(){
         Gdx.input.setInputProcessor(null);
     }
-
 }
+
+
---
+++ core/src/test/SampleTest.java
@@ -0,0 +1,16 @@
+//package org.junit;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import org.junit.Test;
+////import org.junit.*;
+//
+//
+//public class SampleTest {
+//    @Test
+//    public void Test()
+//    {
+//        Assert.assertTrue(true);
+//    }
+//}
